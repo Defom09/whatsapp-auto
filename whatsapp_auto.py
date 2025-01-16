9f1dfe489cb30c954dfba89bdb6e35e772d2a541
Your Name <you@example.com>
2025-01-17 00:00:44 +0100

    cahnge

--- whatsapp_auto.py
+++ whatsapp_auto.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from urllib.parse import quote as url_quote
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
